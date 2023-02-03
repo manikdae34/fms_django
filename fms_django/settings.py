b0e4d5a3a00d5576c1f6288dcf2607d7bc46dd57
Md Manik Sarker <manikdae34@gmail.com>
2023-02-03 22:34:40 +0600

    Static Root Added

--- fms_django/settings.py
+++ fms_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v(!)y2fv58uges^ub#r9n!-oydq=2m%=tv!5b@-x&9lih9^+^t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['f.maniksarker.com', 'www.f.maniksarker.com', '127.0.0.1']
+ALLOWED_HOSTS = ['f.maniksarker.com', 'www.f.maniksarker.com', '127.0.0.1', '27.147.180.28']
 
 
 # Application definition
@@ -119,14 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static",
-    ]
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-#MEDIA
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
