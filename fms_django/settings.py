0d19659ad160c74f8df2f59cf3e4778f8d37c82b
Md Manik Sarker <manikdae34@gmail.com>
2023-02-03 23:02:57 +0600

    static root static

--- fms_django/settings.py
+++ fms_django/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
